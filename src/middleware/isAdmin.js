85c72de13678003a41e1b8d3913438fbacb8b354
ata parvin ghods <ataparvinghods@gmail.com>
2023-03-11 18:29:47 +0330

    Fixed auth middleware

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -1,6 +1,11 @@
 const User = require('../models/user')
 
 module.exports = async (req, res, next) => {
+	console.log(req.headers.telegram_id)
+	if (!req.headers.telegram_id) {
+		return res.status(401).json({ message: 'Unauthorized' })
+	}
+
 	const user = await User.findOne({
 		telegram_id: parseInt(req.headers.telegram_id),
 	})
